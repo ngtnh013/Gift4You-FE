267830a9cbabd56700ba8d9bf84b14038f2ad4c5
hoangbaohuy <144062987+hoangbaohuy@users.noreply.github.com>
2024-10-31 01:22:15 +0700

    Contact Page, Home Page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,5 +1,50 @@
+import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 function ContactPage() {
-    return ( <div>Contact Page</div> );
+    return ( <div className="get-in-touch">
+        <h1>Get In Touch With Us</h1>
+        <p className="pcontent">
+          For More Information About Our Product & Services. Please Feel Free To Drop Us An Email.
+          Our Staff Always Be There To Help You Out. Do Not Hesitate!
+        </p>
+  
+        <div className="contact-container">
+          <div className="contact-info">
+            <div className="info-item">
+              <i className="fas fa-map-marker-alt"></i>
+              <h2>Address</h2>
+              <p>Placeholder address, Vietnam</p>
+            </div>
+  
+            <div className="info-item">
+              <i className="fas fa-phone"></i>
+              <h2>Phone</h2>
+              <p>Mobile: +(84) 546-6789</p>
+              <p>Hotline: +(84) 456-6789</p>
+            </div>
+  
+            <div className="info-item">
+              <i className="fas fa-clock"></i>
+              <h2>Working Time</h2>
+              <p>Monday-Friday: 9:00 - 22:00</p>
+              <p>Saturday-Sunday: 9:00 - 21:00</p>
+            </div>
+          </div>
+  
+          <form className="contact-form">
+            <p className="pdess">Your name</p>
+            <input type="text" placeholder="Abc" required />
+            <p className="pdess">Email address</p>
+            <input type="email" placeholder="Abc@def.com" required />
+            <p className="pdess">Subject</p>
+            <input type="text" placeholder="Subject (This is optional)" />
+            <p className="pdess">Message</p>
+            <textarea placeholder="Hi. I'd like to ask about" required></textarea>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div> );
 }
 
 export default ContactPage;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,64 @@
+import './index.css';
 function HomePage() {
-    return ( <div>home page</div> );
+    return ( <div className="HomeForm">
+        <main className="main">
+            <section className="gift-experience">
+              <div className="left-side">
+                  <div className="title">
+                      <h1 className="create">Create</h1>
+                      <p className="subtitle">Unforgettable</p>
+                      <p className="subtitle">Gift Experience for</p>
+                      <p className="subtitle">Every Occasions</p>
+                  </div>
+                  <div className="description">
+                      <p  className="pdes">Welcome to Gift4U, where you can</p>
+                      <p  className="pdes">design and personalize the perfect gift</p>
+                      <p  className="pdes">box for any special moments,</p>
+                      <p  className="pdes">guaranteed to bring joy to your loved</p>
+                      <p  className="pdes">ones.</p>
+                  </div>
+              </div>
+              <div className="right-side">
+                  <div className="image">
+                  </div>
+              </div>
+            </section>
+  
+            <section className="featuresTitle">
+              <div className="featureTitle">
+                <h1>Choose from a varitey of boc templates to create your perfect gift box</h1>
+              </div>
+            </section>
+  
+            <section className="features">
+              <div className="feature">
+                <h3>Customize your gifts with a personal touch.</h3>
+                <p>Customizable templates to make your gift truly unique.</p>
+              </div>
+              <div className="feature">
+                <h3>A variety of decorations to enhance your gift</h3>
+                <p>Personalize your gift with a heartfelt messages or cherished memories.</p>
+              </div>
+              <div className="feature">
+                <h3>Add a message or photo to make your gift extra special</h3>
+                <p>Explore our selection of extra goodies to make your gift even more special.</p>
+              </div>
+            </section>
+  
+            <section className="gift-box">
+              <div className="box-image">
+                <img src="https://www.foldaboxusa.com/cdn/shop/products/HotPinkSatinonBlackA5Deep_1200x.jpg?v=1700520516" alt="Gift Box" />
+              </div>
+              <div className="box-content">
+                <h2>Discover the</h2>
+                <h2>Perfect Gift Box</h2>
+                <p>Choose from our wide selection of customizable gift boxes for any occasion.</p>
+                <button>Create Now</button>
+              </div>
+            </section>
+  
+        </main>
+      </div> );
 }
 
 export default HomePage;
