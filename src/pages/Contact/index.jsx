6c201bc917eb9df93c5b13c35df367f8910d6475
hoangbaohuy <144062987+hoangbaohuy@users.noreply.github.com>
2024-10-31 14:05:49 +0700

    Contact Page, Home Page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,5 +1,77 @@
+import { FaClock } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
 function ContactPage() {
-    return ( <div>Contact Page</div> );
+    return ( <div className="max-w-3xl mx-auto p-5 text-center font-sans">
+        <h1 className="font-semibold text-4xl mb-3">Get In Touch With Us</h1>
+        <p className="text-lg text-gray-500 mb-5">
+            For More Information About Our Product & Services. Please Feel Free To Drop Us An Email.
+            Our Staff Always Be There To Help You Out. Do Not Hesitate!
+        </p>
+
+        <div className="flex flex-col md:flex-row md:space-x-5">
+            <div className="flex-1 space-y-4 mb-5 md:mb-0">
+                <div className="p-4 border border-gray-300 rounded-lg text-left">
+                    <FaMapMarkerAlt className="text-xl mb-4 block"/>
+                    <h2 className="text-xl">Address</h2>
+                    <p>Placeholder address, Vietnam</p>
+                </div>
+
+                <div className="p-4 border border-gray-300 rounded-lg text-left">
+                    <FaPhone className="text-xl mb-4 block"/>
+                    <h2 className="text-xl">Phone</h2>
+                    <p>Mobile: +(84) 546-6789</p>
+                    <p>Hotline: +(84) 456-6789</p>
+                </div>
+
+                <div className="p-4 border border-gray-300 rounded-lg text-left">
+                    <FaClock className="text-xl mb-4 block"/>
+                    <h2 className="text-xl">Working Time</h2>
+                    <p>Monday-Friday: 9:00 - 22:00</p>
+                    <p>Saturday-Sunday: 9:00 - 21:00</p>
+                </div>
+            </div>
+
+            <form className="flex-1 space-y-3 flex flex-col">
+                <label className="text-left text-xs font-bold">Your name</label>
+                <input
+                    type="text"
+                    placeholder="Abc"
+                    required
+                    className="p-2 border border-gray-300 rounded-lg focus:outline-none"
+                />
+                
+                <label className="text-left text-xs font-bold">Email address</label>
+                <input
+                    type="email"
+                    placeholder="Abc@def.com"
+                    required
+                    className="p-2 border border-gray-300 rounded-lg focus:outline-none"
+                />
+
+                <label className="text-left text-xs font-bold">Subject</label>
+                <input
+                    type="text"
+                    placeholder="Subject (This is optional)"
+                    className="p-2 border border-gray-300 rounded-lg focus:outline-none"
+                />
+
+                <label className="text-left text-xs font-bold">Message</label>
+                <textarea
+                    placeholder="Hi. I'd like to ask about"
+                    required
+                    className="p-2 border border-gray-300 rounded-lg h-32 focus:outline-none"
+                ></textarea>
+
+                <button
+                    type="submit"
+                    className="mt-3 p-2 bg-[#4a3c2e] text-white font-medium rounded-lg hover:bg-[#3a3227]"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    </div> );
 }
 
 export default ContactPage;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,70 @@
+import { FaChevronRight } from "react-icons/fa";
 function HomePage() {
-    return ( <div>home page</div> );
+    return ( <div className="font-sans flex flex-col min-h-screen w-full">
+        <section className="w-full bg-orange-100 flex">
+          <div className="w-1/2 bg-orange-100 p-12 flex flex-col justify-center pl-24 items-start">
+            <div>
+              <h1 className="text-7xl font-bold text-amber-800">Create</h1>
+              <p className="text-5xl font-bold text-amber-800 mt-0">Unforgettable</p>
+              <p className="text-5xl font-bold text-amber-800 mt-0">Gift Experience for</p>
+              <p className="text-5xl font-bold text-amber-800 mt-0">Every Occasions</p>
+            </div>
+            <div className="description mt-8">
+              <p className="text-xl text-left">Welcome to Gift4U, where you can</p>
+              <p className="text-xl text-left mt-0">design and personalize the perfect gift</p>
+              <p className="text-xl text-left mt-0">box for any special moments,</p>
+              <p className="text-xl text-left mt-0">guaranteed to bring joy to your loved</p>
+              <p className="text-xl text-left mt-0">ones.</p>
+            </div>
+          </div>
+          <div className="w-1/2 flex justify-center items-center bg-white">
+            <div
+              className="w-full h-full bg-cover bg-center"
+              style={{ backgroundImage: "url('https://confettigifts.in/cdn/shop/files/openwhen1.jpg?v=1717744384')" }}
+            ></div>
+          </div>
+        </section>
+
+        <section className="flex flex-wrap justify-center mt-12">
+          <div className="mx-6 w-6/12 text-center">
+            <h1 className="font-bold text-4xl">Choose from a variety of box templates to create your perfect gift box</h1>
+          </div>
+        </section>
+
+        <section className="flex flex-wrap justify-center mt-12">
+          <div className="mx-6 w-1/4 text-center">
+            <h3 className="text-2xl font-bold">Customize your gifts with a personal touch.</h3>
+            <p className="text-xl mt-6">Customizable templates to make your gift truly unique.</p>
+          </div>
+          <div className="mx-6 w-1/4 text-center">
+            <h3 className="text-2xl font-bold">A variety of decorations to enhance your gift</h3>
+            <p className="text-xl mt-6">Personalize your gift with heartfelt messages or cherished memories.</p>
+          </div>
+          <div className="mx-6 w-1/4 text-center">
+            <h3 className="text-2xl font-bold">Add a message or photo to make your gift extra special</h3>
+            <p className="text-xl mt-6">Explore our selection of extra goodies to make your gift even more special.</p>
+          </div>
+        </section>
+
+        <section className="w-full flex justify-center mt-12 h-96 bg-orange-100">
+          <div className="mr-12 bg-white w-3/5">
+            <img
+              src="https://www.foldaboxusa.com/cdn/shop/products/HotPinkSatinonBlackA5Deep_1200x.jpg?v=1700520516"
+              alt="Gift Box"
+              className="w-full h-full bg-cover bg-center"
+            />
+          </div>
+          <div className="mt-20 w-2/5">
+            <h2 className="text-5xl font-bold text-amber-800">Discover the</h2>
+            <h2 className="text-5xl font-bold text-amber-800">Perfect Gift Box</h2>
+            <p className="text-2xl my-6 w-3/4">Choose from our wide selection of customizable gift boxes for any occasion.</p>
+            <button className="bg-amber-800 text-white py-2 px-4 text-lg rounded hover:bg-gray-700 flex items-center">
+              Create Now 
+              <FaChevronRight className="ml-2" /> 
+            </button>
+          </div>
+        </section>
+    </div> );
 }
 
 export default HomePage;
