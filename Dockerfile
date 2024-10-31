2636ac353aa0cbbb6bb48579d600aa6cf2871c40
ngtnh013 <tuananh.ngtnh@gmail.com>
2024-10-31 19:13:39 +0700

    update docker hot reload from local

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM node:${NODE_VERSION}-alpine
 
 WORKDIR /app
 
-COPY package*.json .
+COPY package*.json ./
 
 RUN npm install
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Lắng nghe trên tất cả các địa chỉ IP
+    port: 5173,       // Cổng mà Vite sẽ chạy
+    strictPort: true, // Ngăn không cho Vite chuyển sang cổng khác nếu cổng đã chiếm
+    watch: {
+      usePolling: true, // Bật sử dụng polling để phát hiện thay đổi
+      interval: 100,    // Tùy chọn thời gian kiểm tra
+    },
+  },
 })
